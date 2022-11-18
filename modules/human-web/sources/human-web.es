ghostery/common

commit 758cc8ee115fe6bd0a383de63bb85498186d0de7

* Fixed: antitracking should detect Edge (instead of reporting it as Chrome) (#35)
* Include the browser (name + major version) and OS in the "alive" action.
  Skip it more uncommon combinations (i.e. unable of reaching quorum).

modules/human-web/sources/human-web.es
@@ -23,11 +23,12 @@ import DoublefetchHandler from './doublefetch-handler';
 import ContentExtractionPatternsLoader from './content-extraction-patterns-loader';
 import HumanWebPatternsLoader from './human-web-patterns-loader';
 import { ContentExtractor, parseQueryString } from './content-extractor';
+import AliveMessageGenerator from './alive-message-generator';
 import logger from './logger';
 import { parseHtml, getContentDocument } from './html-helpers';
 import { parseURL, Network } from './network';
 import prefs from '../core/prefs';
-import { isEdge } from '../core/platform';
+import isLegacyEdge from '../core/platform';
 import Worker from '../platform/worker';
 import { toUTF8 } from '../core/encoding';
 import pacemaker from '../core/services/pacemaker';
@@ -2832,23 +2833,9 @@ const CliqzHumanWeb = {
           was active or not. We treat the user to be active if in the
           last hour, the user visited two non search pages.
           Get the active usage from DB.
-          hen generate & send payload.
+          Then generate & send payload.
           Update the time last sent, and reset the value to 0.
-
-          Sample payload:
-            {
-            "action": "alive",
-            "ver": "2.7",
-            "type": "humanweb",
-            "payload": {
-            "status": true,
-            "ctry": "de",
-            "t": "2016110909"
-            },
-            "ts": "2016110909"
-            }
         */
-
         const tDiff = parseInt((new Date().getTime() - CliqzHumanWeb.activeUsageLastSent) / 1000);
         if (tDiff > 3600) {
             const activeHours = Object.keys(CliqzHumanWeb.activeUsage);
@@ -2867,20 +2854,20 @@ const CliqzHumanWeb = {
         }
 
     },
-    sendAliveMessage: function(h) {
-        const payload = {
-            status: true,
-            t: h,
-            ctry: CliqzHumanWeb.getCountryCode() // Need to fix this.
-        };
+    async sendAliveMessage(hour) {
+      try {
+        const ctry = CliqzHumanWeb.getCountryCode();
+        const payload = await this.aliveMessageGenerator.generateMessage(ctry, hour);
+        _log('Sending alive message for the hour:', hour, '->', payload);
 
-        _log(`Sending alive message for the hour: ${h} , ${JSON.stringify(payload)}`);
-
-        CliqzHumanWeb.telemetry({
-            type: CliqzHumanWeb.msgType,
-            action: 'alive',
-            payload:payload
+        await CliqzHumanWeb.telemetry({
+          type: CliqzHumanWeb.msgType,
+          action: 'alive',
+          payload,
         });
+      } catch (e) {
+        _log('Failed to send alive signal', e);
+      }
     },
     loadActionStats: function() {
         CliqzHumanWeb.db.loadRecordTelemetry('actionStats', function(data) {
@@ -3248,6 +3235,8 @@ const CliqzHumanWeb = {
         }
         return false;
     },
+
+    // for "page" messages
     async safeQuorumCheck(msg) {
       if (!CliqzHumanWeb.performQC(msg)) {
         _log("Perform QC: quorum check not needed");
@@ -3255,12 +3244,17 @@ const CliqzHumanWeb = {
       }
 
       _log("Perform QC: quorum confirmation required");
-      const hash = await CliqzHumanWeb.sha1(msg.payload.url);
+      return CliqzHumanWeb._quorumCheckAnyString(msg.payload);
+    },
+
+    async _quorumCheckAnyString(str) {
+      const hash = await CliqzHumanWeb.sha1(str);
       await CliqzHumanWeb.sendQuorumIncrement(hash);
 
       const quorumReached = await CliqzHumanWeb.getQuorumConsent(hash);
       return quorumReached;
     },
+
     initWorker: function() {
       if (!CliqzHumanWeb.rushaWorker) {
         CliqzHumanWeb.rushaWorker = new Worker(`${config.baseURL}human-web/rusha.bundle.js`);
@@ -3277,8 +3271,8 @@ const CliqzHumanWeb = {
         };
       }
     },
-    sha1: function(s) {
-      if (isEdge) {
+    sha1(s) {
+      if (isLegacyEdge) {
         CliqzHumanWeb.initWorker();
         return new Promise((resolve, reject) => {
           const id = Math.random() * (2 ** 53);
@@ -3419,7 +3413,7 @@ const CliqzHumanWeb = {
     },
     getCountryCode: function() {
         let ctryCode = prefs.get('config_location', null);
-        return CliqzHumanWeb.sanitizeCounrtyCode(ctryCode);
+        return CliqzHumanWeb.sanitizeCountryCode(ctryCode);
     },
     getTS: function() {
         try {
@@ -3429,7 +3423,7 @@ const CliqzHumanWeb = {
           return null;
         }
     },
-    sanitizeCounrtyCode: function(ctryCode){
+    sanitizeCountryCode: function(ctryCode){
         let _countryCode = ctryCode;
         if(allowedCountryCodes.indexOf(_countryCode) === -1) {
             _countryCode = '--';
@@ -3840,5 +3834,28 @@ const CliqzHumanWeb = {
   }
 };
 CliqzHumanWeb.contentExtractor = new ContentExtractor(CliqzHumanWeb);
+
+const initAliveMessageGenerator = () => {
+  const quorumChecker = (stringifiedConfig) => {
+    return CliqzHumanWeb._quorumCheckAnyString(stringifiedConfig);
+  };
+  const aliveConfigStorage = {
+    async load() {
+      if (!CliqzHumanWeb.db) {
+        return null;
+      }
+      return CliqzHumanWeb.db.loadAliveConfig();
+    },
+    async store(config, hasQuorum) {
+      if (!CliqzHumanWeb.db) {
+        return;
+      }
+      await CliqzHumanWeb.db.storeAliveConfig(config, hasQuorum);
+      return;
+    }
+  };
+  return new AliveMessageGenerator(quorumChecker, aliveConfigStorage);
+};
+CliqzHumanWeb.aliveMessageGenerator = initAliveMessageGenerator();
 
 export default CliqzHumanWeb;

modules/antitracking/sources/background.es
@@ -20,7 +20,7 @@ import Config, { TELEMETRY } from './config';
 import { updateTimestamp } from './time';
 import { bindObjectFunctions } from '../core/helpers/bind-functions';
 import inject from '../core/kord/inject';
-import { isEdge } from '../core/platform';
+import { isLegacyEdge } from '../core/platform';
 import { parse } from '../core/url';
 
 // Telemetry schemas
@@ -74,7 +74,7 @@ export default background({
 
     // load config
     this.config = new Config({}, () => this.core.action('refreshAppState'));
-    if (isEdge) {
+    if (isLegacyEdge) {
       this.config.databaseEnabled = false;
     }
     this.attrack.webRequestPipeline.action('getPageStore').then((pageStore) => {

modules/antitracking/sources/telemetry.es
@@ -27,10 +27,12 @@ function getPlatformConstants() {
     let userAgent = '';
     if (pltfrm.isFirefox) {
       userAgent = 'firefox';
-    } else if (pltfrm.isChromium) {
-      userAgent = 'chrome';
     } else if (pltfrm.isEdge) {
       userAgent = 'edge';
+    } else if (pltfrm.isLegacyEdge) {
+      userAgent = 'legacy-edge';
+    } else if (pltfrm.isChromium) {
+      userAgent = 'chrome';
     }
     return {
       platform: pltfrm.isMobile ? 'mobile' : '',

modules/core/sources/platform.es
@@ -26,6 +26,7 @@ export const isFirefox = platform.isFirefox;
 export const isMobile = platform.isMobile || Boolean(config.isMobile);
 export const isChromium = platform.isChromium;
 export const isEdge = platform.isEdge;
+export const isLegacyEdge = platform.isLegacyEdge;
 export const platformName = platform.platformName;
 export const isCliqzBrowser = channel === '40' || channel === '99';
 export const isGhosteryExtension = channel.startsWith('CH');

modules/core/sources/webrequest.es
@@ -7,7 +7,7 @@
  */
 
 import webrequest, { VALID_RESPONSE_PROPERTIES } from '../platform/webrequest';
-import { isEdge } from './platform';
+import { isLegacyEdge } from './platform';
 
 export default webrequest;
 
@@ -34,7 +34,7 @@ function getOptionArray(options) {
 }
 
 // build allowed extraInfo options from <Step>Options objects.
-export const EXTRA_INFO_SPEC = !isEdge ? {
+export const EXTRA_INFO_SPEC = !isLegacyEdge ? {
   onBeforeRequest: getOptionArray(webrequest.OnBeforeRequestOptions),
   onBeforeSendHeaders: getOptionArray(webrequest.OnBeforeSendHeadersOptions),
   onSendHeaders: getOptionArray(webrequest.OnSendHeadersOptions),

modules/human-web/sources/alive-message-generator.es
@@ -1,0 +1,106 @@
+/*!
+ * Copyright (c) 2014-present Cliqz GmbH. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import UAParser from 'ua-parser-js';
+import logger from './logger';
+
+/**
+ * Responsible for generating information to be included in the "alive" message.
+ * The purpose of the additional information in the alive message is to detect
+ * unusual traffic, for instance, if a new browser version introduced a regression.
+ *
+ * The information will only be included if it has been seen by multiple users.
+ * The data itself should not be sensitive, still there is no point in including it
+ * as only "big hitters" are interesting. For instance, should there be a problem in
+ * an obscure setup, it is less interesting then if a recent Firefox or Chrome release
+ * breaks something on Windows.
+ */
+export default class AliveMessageGenerator {
+  constructor(quorumChecker, aliveConfigStorage) {
+    this.quorumChecker = quorumChecker;
+    this.aliveConfigStorage = aliveConfigStorage;
+  }
+
+  async generateMessage(countryCode, hour) {
+    const parser = UAParser();
+    parser.browser = parser.browser || {};
+    parser.os = parser.os || {};
+
+    const browser = parser.browser.name || '';
+    const os = parser.os.name || '';
+
+    // extracts only the major version (e.g. "96.0.1" -> 96)
+    let version = parseInt(parser.browser.version, 10);
+    if (isNaN(version)) {
+      version = '';
+    }
+
+    const config = {
+      browser,
+      version, // major version
+      os,
+      ctry: countryCode, // sanitized country ('--' for countries with a small population)
+    };
+    try {
+      const reachedQuorum = await this._reachedQuorum(config);
+      if (reachedQuorum) {
+        config.t = hour;
+        logger.debug('Found the following configuration safe to share:', config);
+        return config;
+      }
+    } catch (e) {
+      logger.warn('Failed to confirm that the configuration is popular enough:', e);
+    }
+
+    logger.debug('Omitting the config, as it is not popular enough:', config);
+    return {
+      browser: '',
+      version: '',
+      os: '',
+      ctry: '--',
+      t: hour,
+    };
+  }
+
+  /**
+   * Checks that the additional configuration is popular enough to be shared.
+   * As long as the configuration does not change, keep the initial result of the
+   * quorum check. Otherwise, it might happen that it reaches quorum over time
+   * by itself (provided that we keep sharing it and it never changes).
+   *
+   * If there is a false positive (a popular config being not detected), it will
+   * eventually recover as the configuration will changed over time (e.g. after each
+   * browser update).
+   */
+  async _reachedQuorum(config) {
+    const newConfig = this._deterministicStringify(config);
+    let reachedQuorum = null;
+
+    try {
+      const oldValue = await this.aliveConfigStorage.load();
+      if (oldValue && newConfig === oldValue.config) {
+        reachedQuorum = oldValue.reachedQuorum;
+      }
+    } catch (e) {
+      logger.warn('Ignore errors when reading the old state, but check the quorum again', e);
+    }
+
+    if (reachedQuorum === null) {
+      reachedQuorum = await this.quorumChecker(newConfig);
+      await this.aliveConfigStorage.store({
+        config: newConfig,
+        reachedQuorum,
+      });
+    }
+    return reachedQuorum;
+  }
+
+  _deterministicStringify(config) {
+    return JSON.stringify(Object.keys(config).sort().map(x => config[x]));
+  }
+}

modules/human-web/sources/human-web-patterns-loader.ts
@@ -6,7 +6,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import { isEdge } from '../core/platform';
+import { isLegacyEdge } from '../core/platform';
 import {
   RemoteResourceWatcher,
   ResourceUpdatedCallback
@@ -36,7 +36,7 @@ ZpJDPOb9RzbPMOawfjPhkayRb9yvU0f6pLvKm6kmKLsoblsaeLFbhu1igO8nXP4X
 -----END PUBLIC KEY-----`,
   },
 
-  // fallback for Edge
+  // fallback for legacy Edge (before Chrome)
   '2019-10-24-human-web-edge.pub': {
     algorithm: 'RSASSA-PKCS1-v1_5',
     pem: `-----BEGIN PUBLIC KEY-----
@@ -74,7 +74,7 @@ export default class SignedPatternsLoader {
 
   constructor(url: string, onUpdate: ResourceUpdatedCallback) {
     const publicKeyName =
-      isEdge ? '2019-10-24-human-web-edge.pub' : '2019-10-24-human-web.pub';
+      isLegacyEdge ? '2019-10-24-human-web-edge.pub' : '2019-10-24-human-web.pub';
     const { algorithm, pem } = trustedSigningKeys[publicKeyName];
 
     this.verifier = new SignatureVerifier({

modules/human-web/tests/integration/human-web-test.es
@@ -88,13 +88,13 @@ export default function () {
 
       notAllowed.forEach((e) => {
         it(`'${e}'is not allowed`, function () {
-          expect(CliqzHumanWeb.sanitizeCounrtyCode(e)).to.equal('--');
+          expect(CliqzHumanWeb.sanitizeCountryCode(e)).to.equal('--');
         });
       });
 
       allowed.forEach((e) => {
         it(`'${e}' is allowed`, function () {
-          expect(CliqzHumanWeb.sanitizeCounrtyCode(e)).to.equal(e);
+          expect(CliqzHumanWeb.sanitizeCountryCode(e)).to.equal(e);
         });
       });
     });

commit 16cb59ffbf839c5fafb20e36ce5bc06844871616

Fixed: when page messages ask for quorum, the hash should be computed from the URL not the payload. (#71)
Trying to hash the object will fail and messages will be mistakenly dropped.

The regression was introduced in https://github.com/ghostery/common/pull/35/

modules/human-web/sources/human-web.es
@@ -3244,7 +3244,7 @@ const CliqzHumanWeb = {
       }
 
       _log("Perform QC: quorum confirmation required");
-      return CliqzHumanWeb._quorumCheckAnyString(msg.payload);
+      return CliqzHumanWeb._quorumCheckAnyString(msg.payload.url);
     },
 
     async _quorumCheckAnyString(str) {